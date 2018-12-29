9fa5c4fd65861c0c3fafba8f1b6eead8038a9cd2
vivek.shringi <shringi.vivek@gmail.com>
2018-12-29 20:17:43 +0100

    updated the content

--- src/test/java/internet/AppTest.java
+++ src/test/java/internet/AppTest.java
@@ -1,6 +1,16 @@
+package internet;
+
+import java.awt.AWTException;
+import java.awt.RenderingHints.Key;
+import java.awt.Robot;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -14,9 +24,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -28,13 +38,32 @@ import org.testng.annotations.Test;
 
 public class AppTest {
 	WebDriver driver;
-	static final String URL="http://the-internet.herokuapp.com";
+	static final String URL="http://localhost:9292";
 	static int count =0;
+	String downloadLoc = "/Users/vivshr/Downloads/";
 	
 	
 	@BeforeClass
 	public void setUp(){
-		driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.gecko.driver","geckodriver");
+		FirefoxOptions options= new FirefoxOptions();
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setAcceptUntrustedCertificates(true);
+		profile.setAssumeUntrustedCertificateIssuer(false);
+		//Set Location to store files after downloading.
+		profile.setPreference("browser.download.dir", downloadLoc);
+		profile.setPreference("browser.download.folderList", 2);
+ 
+		//Set Preference to not show file download confirmation dialogue using MIME types Of different file extension types.
+		profile.setPreference("browser.helperApps.neverAsk.saveToDisk", 
+		    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;txt"); 
+		profile.setPreference("browser.helperApps.neverAsk.saveToDisk","application/pdf,application/x-pdf,application/octet-stream");
+		profile.setPreference( "browser.download.manager.showWhenStarting", false );
+		profile.setPreference( "pdfjs.disabled", true );
+        options.setProfile(profile);
+        driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.get(URL);
 	}
 	
@@ -46,7 +75,6 @@ public class AppTest {
 	
 	//@Test
 	public void brokenImageVerification() throws ClientProtocolException, IOException{
-		
 		driver.navigate().to(URL+"/broken_images");
 		List <WebElement> images = driver.findElements(By.tagName("img"));
 		for(WebElement l : images){
@@ -57,7 +85,6 @@ public class AppTest {
 			  System.out.println(l.getAttribute("src")+" is  a Broken image");
 			  count++;
 			}
-			
 		}
 		System.out.println(count+" images are broken out of "+images.size());
 	}
@@ -75,9 +102,6 @@ public class AppTest {
 	System.out.println(driver.findElement(By.id("canvas")).getText());
 	System.out.println(driver.findElement(By.id("canvas")).getLocation());
 	System.out.println(driver.findElement(By.id("canvas")).getSize());
-	 //((JavascriptExecutor)driver).executeScript("document.");
-	//To do here is how to get text from canvas and verify. to get canvas text we have perform java script method what exactly i have to use that needs to be checked"
-	// How to convert rgba value to hex code
 	}
 	}
 	
@@ -91,22 +115,24 @@ public class AppTest {
 		}
 	
 	}
-	
+	// This scenarios is not working with java though support is available using ruby
 	//@Test
 	public void contextMenu() throws InterruptedException{
 		driver.navigate().to(URL+"/context_menu"); 
         Actions action= new Actions(driver);
-        action.contextClick(driver.findElement(By.id("hot-spot"))).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ENTER).build().perform();
+        action.contextClick(driver.findElement(By.id("hot-spot")))
+        .sendKeys(Keys.ARROW_DOWN)
+        .sendKeys(Keys.ARROW_DOWN)
+        .sendKeys(Keys.ARROW_DOWN)
+        .sendKeys(Keys.ARROW_DOWN)
+        .sendKeys(Keys.ENTER)
+        .build().perform();
+        Thread.sleep(2000);
         Alert alert = driver.switchTo().alert();
         Assert.assertEquals(alert.getText(), "You selected a context menu");
         alert.accept();
         Assert.assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Context Menu");
+        Thread.sleep(10000);
         
 	}
 	
@@ -120,9 +146,7 @@ public class AppTest {
 			   String bef_1=a.getCssValue("color");
 			   String bef_2=a.getCssValue("font-size");
 			   Actions action= new Actions(driver);
-			   Thread.sleep(5000);
 			   action.moveToElement(a).build().perform();
-			   Thread.sleep(5000);
 			   String aft_1=a.getCssValue("color");
 			   String aft_2=a.getCssValue("font-size");
 			   Assert.assertFalse(bef_1.equals(aft_1));
@@ -132,25 +156,32 @@ public class AppTest {
 	
 	}
 	
-	//@Test
-	public void drag_and_dropTest() throws InterruptedException{
+	//@Test //This scenario is not working and needs more investigation
+	public void drag_and_dropTest() throws InterruptedException, FileNotFoundException{
 		driver.navigate().to(URL+"/drag_and_drop");
-                WebElement source= driver.findElement(By.id("column-a"));
+		WebElement source= driver.findElement(By.id("column-a"));
         WebElement target= driver.findElement(By.id("column-b"));
-       System.out.println(source.getAttribute("style"));
-       System.out.println(source.getLocation());
-       Actions a= new Actions(driver);
-       a.moveByOffset(240, 90);
-       a.clickAndHold(source);
-       a.moveToElement(source,490, 90);
-       a.release();
-       a.build().perform();
-        //new Actions(driver).dragAndDropBy(source, 400, 400).build().perform();
-       // new Actions(driver).dragAndDrop(source, target).build().perform();
-        //new Actions(driver).clickAndHold(source).moveToElement(source, 400, 400).build().perform();
-        //new Actions(driver).moveToElement(source).clickAndHold().moveToElement(source,215,0).release().build().perform();
-        
-        Thread.sleep(2000);
+       
+        Actions a= new Actions(driver);
+        a.dragAndDrop(source, target).build().perform();
+       
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		Scanner sc = new Scanner(new FileInputStream(new File("dnd.js")));
+		String inject = ""; 
+		    while (sc.hasNext()) {          
+		        String[] s = sc.next().split("\r\n");   
+		        for (int i = 0; i < s.length; i++) {
+		            inject += s[i];
+		            inject += " ";
+		        }           
+		    } 
+		    System.out.println(inject);
+		    js.executeAsyncScript(inject);
+		    
+        Thread.sleep(2000);  
+       Assert.assertEquals(source.getText(),"B");  
+       Assert.assertEquals(target.getText(),"A");
 		}
 	
 	//@Test
@@ -173,8 +204,8 @@ public class AppTest {
 		driver.navigate().to(URL+"/dynamic_content");
 		List <WebElement> images = driver.findElements(By.xpath("//div/img"));
 		for(WebElement j : images){
-		    System.out.println(j.getAttribute("src").subSequence(84, 85));
-			Assert.assertTrue(Arrays.asList("1", "2", "3","4","5","6","7").contains(j.getAttribute("src").subSequence(84, 85)));
+		    System.out.println(j.getAttribute("src").subSequence(72, 73));
+			Assert.assertTrue(Arrays.asList("1", "2", "3","4","5","6","7").contains(j.getAttribute("src").subSequence(72, 73)));
 			
 		}
 		}
@@ -184,14 +215,14 @@ public class AppTest {
 	public void dynamicControls() {
 		driver.navigate().to(URL+"/dynamic_controls");
         driver.findElement(By.id("checkbox")).click();
-        Assert.assertEquals(driver.findElement(By.id("btn")).getText(), "Remove");
-        driver.findElement(By.id("btn")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("#checkbox-example > button")).getText(), "Remove");
+        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("loading"))); 
         String a = driver.findElement(By.id("message")).getText();
         Assert.assertEquals(a, "It's gone!");
-        Assert.assertEquals(driver.findElement(By.id("btn")).getText(), "Add");
-        driver.findElement(By.id("btn")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("#checkbox-example > button")).getText(), "Add");
+        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("loading"))); 
        a = driver.findElement(By.id("message")).getText();
         Assert.assertEquals(a, "It's back!");
@@ -202,7 +233,7 @@ public class AppTest {
 		driver.navigate().to(URL+"/dynamic_loading");
 		driver.findElement(By.linkText("Example 1: Element on page that is hidden")).click();
 		String s = driver.getCurrentUrl();
-		Assert.assertEquals(s, "http://the-internet.herokuapp.com/dynamic_loading/1");
+		Assert.assertEquals(s, "http://localhost:9292/dynamic_loading/1");
 		System.out.println(driver.findElement(By.cssSelector("#finish > h4")).getText());
 		driver.findElement(By.cssSelector("button")).click();
 	    WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -211,28 +242,95 @@ public class AppTest {
 		driver.navigate().to(URL+"/dynamic_loading");
 		driver.findElement(By.linkText("Example 2: Element rendered after the fact")).click();
 		String s1 = driver.getCurrentUrl();
-		Assert.assertEquals(s1, "http://the-internet.herokuapp.com/dynamic_loading/2");
+		Assert.assertEquals(s1, "http://localhost:9292/dynamic_loading/2");
 		driver.findElement(By.cssSelector("button")).click();
 	    wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("loading"))); 
 		System.out.println(driver.findElement(By.cssSelector("#finish > h4")).getText());
 	}
 	
-	@Test
-	public void exitIntent() {
+	//@Test(groups = {"inProgeress"}) //Not working 
+	public void exitIntent() throws AWTException {
 		driver.navigate().to(URL+"/exit_intent");
 		Assert.assertEquals(driver.getTitle(), "The Internet");
-		Actions act = new Actions(driver);
-		act.moveByOffset(0, 0);
+		/*Actions act = new Actions(driver);
+		act.moveByOffset(0, 0).build().perform();*/
+		Robot r = new Robot();
+		r.mouseMove(0, 0);
+		r.delay(1000);
+		
 		Alert alert = driver.switchTo().alert();
 		alert.accept();
-		
-
 	}
 	
+	//@Test
+	public void testFileDownload() throws InterruptedException {
+		driver.navigate().to(URL+ "/download");
+	    WebElement W = driver.findElement(By.cssSelector("#content > div > a:nth-child(2)"));
+		System.out.println(downloadLoc + W.getText());
+		W.click();
+		Thread.sleep(2000);
+		File downloadedFile = new File(downloadLoc + W.getText());
+	    Assert.assertTrue(downloadedFile.exists());
+	}
+	
+	//@Test
+	public void testFileUpload() {
+		driver.navigate().to(URL+ "/upload");
+		String filePath = System.getProperty("user.dir") + "/_50.png";
+		System.out.println(filePath);
+		driver.findElement(By.id("file-upload")).sendKeys(filePath);
+		driver.findElement(By.id("file-submit")).click();
+		String upload_file = driver.findElement(By.id("uploaded-files")).getText();
+		Assert.assertEquals(upload_file, "_50.png");
+	}
+	
+	//@Test
+	public void floatingMenu() throws InterruptedException {
+		driver.navigate().to(URL+"/floating_menu");
+		driver.manage().window().maximize();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String script = "window.scrollBy(0,1000)";
+		js.executeScript(script);
+		 List<WebElement> topMenuItems = driver.findElements(By.cssSelector("li"));
+		 for(WebElement element : topMenuItems){
+			   System.out.println(element.getText());
+			   element.click();
+			}
+	}
+	
+	//@Test SMTP needs to configured
+	public void forgotPassword() throws InterruptedException {
+		driver.navigate().to(URL+"/forgot_password");
+		driver.findElement(By.id("email")).sendKeys("hello@giikihello.com");
+		driver.findElement(By.cssSelector(".icon-2x.icon-signin")).click();
+	    Thread.sleep(10000);
+	}
+	
+    //@Test
+    public void formAuthentication() {
+    	driver.navigate().to(URL+"/login");
+    	driver.findElement(By.id("username")).sendKeys("tomsmith");
+    	driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+    	driver.findElement(By.cssSelector("button.radius")).click();
+    	String s = driver.findElement(By.id("flash")).getText();
+    	Assert.assertEquals(s, "You logged into a secure area!\n" + 
+    			"×");
+    }
+    
+    @Test
+    public void framesTest() {
+    	driver.navigate().to(URL+"/nested_frames");
+    	driver.switchTo().frame("frame-left");
+    	WebElement w = driver.findElement(By.tagName("body"));
+    	System.out.println(w.getText());
+ 
+    	
+    	
+    	
+    }
 	
 	@AfterClass
 	public void tearUp(){
-	    driver.close();
 	    driver.quit();
 	}
 	
